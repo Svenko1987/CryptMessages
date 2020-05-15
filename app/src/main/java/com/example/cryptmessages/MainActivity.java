b62f4cbd50408048886c8b42c4550113ac334eac
Svenko <celikovicsven@gmail.com>
2020-05-15 14:07:43 +0200

    Added cipher Share button and app is prevented from taking screen shots

--- app/src/main/java/com/example/cryptmessages/MainActivity.java
+++ app/src/main/java/com/example/cryptmessages/MainActivity.java
@@ -5,52 +5,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.cryptmessages.controls.cipherMethods.Decrypt;
 import com.example.cryptmessages.controls.cipherMethods.Encrypt;
-import com.example.cryptmessages.controls.editingButtons.Clipboard;
+
 
 public class MainActivity extends AppCompatActivity {
-    EditText keyTa, cipherTextTa,plainTextTa;
-    Button encryptBtn, decryptBtn, copyBtn, pasteBtn, clearBtn,seeKeyBtn;
+    EditText keyTa, cipherTextTa, plainTextTa;
+    Button encryptBtn, decryptBtn, copyBtn, pasteBtn, shareBtn, clearBtn, seeKeyBtn;
 
     Decrypt decrypt = new Decrypt();
     Encrypt encrypt = new Encrypt();
     private ClipboardManager clipboardManager;
     private ClipData clipData;
-    boolean vis=false;
-
+    boolean vis = false;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
         setContentView(R.layout.activity_main);
 
+
         cipherTextTa = findViewById(R.id.cipherTextTa);
-        plainTextTa= findViewById(R.id.plainTextTA);
+        plainTextTa = findViewById(R.id.plainTextTA);
         keyTa = findViewById(R.id.keyTa);
         encryptBtn = findViewById(R.id.encryptBtn);
         decryptBtn = findViewById(R.id.decryptBtn);
         copyBtn = findViewById(R.id.copyBtn);
         pasteBtn = findViewById(R.id.pasteBtn);
+        shareBtn = findViewById(R.id.shareBtn);
         clearBtn = findViewById(R.id.clearBtn);
-        seeKeyBtn=findViewById(R.id.seeKeyBtn);
-        clipboardManager = (ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
+        seeKeyBtn = findViewById(R.id.seeKeyBtn);
+        clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
 
         encryptBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
 
-                        String temp = plainTextTa.getText().toString();
-                        cipherTextTa.setText(encrypt.encrypt(temp, keyTa.getText().toString()));
+                    String temp = plainTextTa.getText().toString();
+                    cipherTextTa.setText(encrypt.encrypt(temp, keyTa.getText().toString()));
 
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -73,9 +77,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String txtcopy = cipherTextTa.getText().toString();
-                clipData = ClipData.newPlainText("text",txtcopy);
+                clipData = ClipData.newPlainText("text", txtcopy);
                 clipboardManager.setPrimaryClip(clipData);
-                Toast.makeText(getApplicationContext(),"Data Copied to Clipboard", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Data Copied to Clipboard", Toast.LENGTH_SHORT).show();
             }
         });
         pasteBtn.setOnClickListener(new View.OnClickListener() {
@@ -85,7 +89,24 @@ public class MainActivity extends AppCompatActivity {
                 ClipData.Item item = pData.getItemAt(0);
                 String txtpaste = item.getText().toString();
                 cipherTextTa.setText(txtpaste);
-                Toast.makeText(getApplicationContext(),"Data Pasted from Clipboard",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Data Pasted from Clipboard", Toast.LENGTH_SHORT).show();
+            }
+        });
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (cipherTextTa.getText().toString().equals("")) {
+                    Toast.makeText(getApplicationContext(), "Cipher field is empty", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(Intent.ACTION_SEND);
+                    intent.setType("text/plain");
+                    String shareBody = cipherTextTa.getText().toString();
+                    intent.putExtra(Intent.EXTRA_SUBJECT, shareBody);
+                    intent.putExtra(Intent.EXTRA_TEXT, shareBody);
+                    startActivity(Intent.createChooser(intent, "Share using"));
+
+                }
+
             }
         });
         clearBtn.setOnClickListener(new View.OnClickListener() {
@@ -100,12 +121,12 @@ public class MainActivity extends AppCompatActivity {
         seeKeyBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!vis){
+                if (!vis) {
                     keyTa.setTransformationMethod(null);
-                    vis=true;
-                }else{
+                    vis = true;
+                } else {
                     keyTa.setTransformationMethod(new PasswordTransformationMethod());
-                    vis=false;
+                    vis = false;
                 }
 
             }
